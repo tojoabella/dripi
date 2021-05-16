98ae76cef6df02a342b9e693ac1c76bbc260f69d
Tojo Abella <terricabella@gmail.com>
2021-05-16 10:23:39 -1000

    power button and mode changer button working

--- pi/parts/buttontest.c
+++ pi/parts/buttontest.c
@@ -6,6 +6,16 @@ static int cont = 1;
 
 #define led 17
 #define button 23
+#define buttonPower 24
+
+int modeMax = 3;
+int mode = 1;
+
+int buttonState = 0;
+int lastReading = 0;
+int currentReading = 0;
+long timeChanged;
+long timePress = 50;
 
 void setup(){
 	pinSetup();
@@ -18,17 +28,60 @@ void cleanup(int sig){
 	pinMode(led, INPUT);
 }
 
-int main(void){
-	signal(SIGINT, cleanup);
-	setup();
+void ledOn(){
+	digitalWrite(led, 1);
+}
 
-	while(cont){
-		if (digitalRead(button)){
-			digitalWrite(led, 1);
-			delay(1000);
-			digitalWrite(led, 0);
-			delay(1000);
-		}
+void ledBlink(){
+	for (int i =  0; i < 3; i++){
+		digitalWrite(led, 0);
+		delay(200);
+		digitalWrite(led, 1);
+		delay(200);
 	}
+}
+
+void ledOff(){
+	digitalWrite(led, 0);
+}
+
+void powerCheck(){
+	if (digitalRead(buttonPower)){
+		cleanup(2);
+	}
+}
+
+int main(void){
+ 	signal(SIGINT, cleanup);
+	setup();
+	ledOn();
+	while(cont){
+		powerCheck();
+		
+		currentReading = digitalRead(button);
+		if(currentReading != lastReading){
+			timeChanged = millis();
+		}
+		if(millis() - timeChanged > 50){
+			if (currentReading != buttonState){
+				buttonState = currentReading;
+				if (buttonState){
+					mode = mode%modeMax + 1;
+					printf("mode: %d\n", mode);
+					if (mode == 1){
+						ledOn();
+					}
+					else if (mode == 2){
+						ledBlink();
+					}
+					else{
+						ledOff();
+					}
+				}
+			}
+		}
+		lastReading = currentReading;
+	}
+
 	return 0;
 }
