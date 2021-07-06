6a2fd1bc25ea2acbdd57abbe5b7f1a949500c04e
Terric Abella <tojo@tojo.local>
2021-07-05 15:04:21 -1000

    imports working

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-/*import themeToggler from './theme.js';*/
+import {themeToggler} from './theme.js';
 
 function visitPageNewWindow(link){
     window.open(link);
@@ -9,17 +9,4 @@ function scrollTo(id){
 }
 
 /*light and dark theme*/
-const theme = document.querySelector('.btn2');
-theme.addEventListener('click', function(){
-    let bodyClassName = document.body.className;
-    if (bodyClassName == "light-theme") {
-        this.textContent = "Dark";
-        document.body.classList.add('dark-theme');
-        document.body.classList.remove('light-theme');
-    }
-    else{
-        this.textContent = "Light";
-        document.body.classList.add('light-theme');
-        document.body.classList.remove('dark-theme');
-    }
-});
+themeToggler();
--- web/theme.js
+++ web/theme.js
@@ -1,13 +1,18 @@
-export default function themeToggler() {
-    let bodyClassName = document.body.className;
-    if (bodyClassName == "light-theme") {
-        this.textContent = "Dark";
-        document.body.classList.add('dark-theme');
-        document.body.classList.remove('light-theme');
-    }
-    else{
-        this.textContent = "Light";
-        document.body.classList.add('light-theme');
-        document.body.classList.remove('dark-theme');
-    }
+function themeToggler() {
+    const theme = document.querySelector('.btn2');
+    theme.addEventListener('click', function(){
+        let bodyClassName = document.body.className;
+        if (bodyClassName == "light-theme") {
+            this.textContent = "Dark";
+            document.body.classList.add('dark-theme');
+            document.body.classList.remove('light-theme');
+        }
+        else{
+            this.textContent = "Light";
+            document.body.classList.add('light-theme');
+            document.body.classList.remove('dark-theme');
+        }
+    });
 }
+
+export { themeToggler }
