85cdc4d50eae12b5f40367d99bc33753c4d9c3a6
Terric Abella <tojo@tojo.local>
2021-07-05 17:19:02 -1000

    user-specific modes

--- web/app.js
+++ web/app.js
@@ -2,11 +2,11 @@ import {themeToggler} from './theme.js';
 
 function visitPageNewWindow(link){
     window.open(link);
-}
+};
 
 function scrollTo(id){
     document.getElementById('led').scrollIntoView();
-}
+};
 
 /*light and dark theme*/
 themeToggler();
