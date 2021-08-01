ef2a58bd1dcc25fa16671d804fd1373382c325b7
Terric Abella <tojo@tojo.local>
2021-07-31 16:31:47 -1000

    dropdown menu top bar

--- web/components/top_bar.js
+++ web/components/top_bar.js
@@ -2,13 +2,22 @@ export default function top_bar(title) {
    let topBar = document.getElementById("top_bar");
    topBar.innerHTML = ('\
    <div class=top_bar>\
-     <a class="item_relative_right" href="#">Log in</a>\
-     <div class="inline_container item_absolute_vertically_center settings_icon_pos" onclick="myFunction(this)">\
-        <div class="settings_icon"></div>\
-        <div class="settings_icon"></div>\
-        <div class="settings_icon"></div>\
-     </div>\
-     <h1>' + title + '</h1>\
-  </div>\
+      <a class="item_relative_right" href="#">Log in</a>\
+      \
+      <div class="item_relative_right">\
+         <button class="dropbtn" onclick="myFunction()">Dropdown</button>\
+         <div class="dropdown-content" id="myDropdown">\
+            <a href="#">Link 1</a>\
+            <a href="#">Link 2</a>\
+         </div>\
+      </div>\
+      <h1>' + title + '</h1>\
+   </div>\
    ');
 };
+
+//<div class="inline_container item_absolute_vertically_center">\
+//</div>\
+// <div class="settings_icon"></div>\
+//         <div class="settings_icon"></div>\
+ //        <div class="settings_icon"></div>\
